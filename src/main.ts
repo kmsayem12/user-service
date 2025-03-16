e6f02352c62c4ce5d008932a2782e438bbbf276f
kmsayem12 <sayem312518@gmail.com>
2025-03-16 16:07:35 +0600

    project init

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  // const app = await NestFactory.create(AppModule);
+  // await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 6001,
+      },
+    },
+  );
+  await app.listen();
+}
+bootstrap();
